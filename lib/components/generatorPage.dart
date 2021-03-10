871dd5495d66faee8d24633634964e733a35689e
STARLORD3 <nileshsingh217@gmail.com>
2021-03-10 21:38:34 +0530

    updated some UI/UX

--- lib/components/generatorPage.dart
+++ lib/components/generatorPage.dart
@@ -1,23 +1,20 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:forest_tagger/components/backButton.dart';
 import 'package:forest_tagger/components/qrshower.dart';
 import 'package:forest_tagger/main.dart';
 import 'package:uuid/uuid.dart';
 
-class GeneratorPage extends StatefulWidget{
-
+class GeneratorPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return GeneratorPageState();
   }
-  
 }
 
-class GeneratorPageState extends State<GeneratorPage>{
-
+class GeneratorPageState extends State<GeneratorPage> {
   String dataToFeed = "";
   TextEditingController controllerloc = TextEditingController();
   TextEditingController controllerlen = TextEditingController();
@@ -26,106 +23,174 @@ class GeneratorPageState extends State<GeneratorPage>{
 
   @override
   Widget build(BuildContext context) {
-
     return Material(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: 300,
-              child: TextFormField(
-                controller: controllername,
-                decoration: InputDecoration(
-                    labelText: 'Enter name of the tree species',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.nature_people)
+          backButton(),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage("assets/images/palm-tree-outline.png"),
+                  height: 200,
+                  width: 100,
+                  color: Colors.green,
                 ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: 300,
-              child: TextFormField(
-                controller: controlleryear,
-                decoration: InputDecoration(
-                    labelText: 'Enter year of plantation of the tree',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.timelapse)
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: Wrap(
+                    children: [
+                      Text(
+                        "Enter the details to generate a QR code :",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: 300,
-              child: TextFormField(
-                controller: controllerloc,
-                decoration: InputDecoration(
-                  labelText: 'Enter latitude, longitude',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.location_on)
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: 300,
-              child: TextFormField(
-                controller: controllerlen,
-                decoration: InputDecoration(
-                    labelText: 'Enter max height of the tree',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.height)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 300,
+                    child: TextFormField(
+                      controller: controllername,
+                      decoration: InputDecoration(
+                          labelText: 'Enter name of the tree species',
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.nature_people)),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ),
-          FlatButton(
-            child: Text("Generate QR code", style: TextStyle(color: Colors.white),),
-            color: Colors.green,
-            onPressed: () async{
-              if(controllerloc.text.isNotEmpty && controllerlen.text.isNotEmpty && controlleryear.text.isNotEmpty && controllername.text.isNotEmpty){
-                String treeId = Uuid().v4();
-                String treeName = controllername.text;
-                String treeYear = controlleryear.text;
-                String loc = controllerloc.text;
-                String treeMaxLen = controllerlen.text;
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 300,
+                    child: TextFormField(
+                      controller: controlleryear,
+                      decoration: InputDecoration(
+                          labelText: 'Enter year of plantation of the tree',
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.timelapse)),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 300,
+                    child: TextFormField(
+                      controller: controllerloc,
+                      decoration: InputDecoration(
+                          labelText: 'Enter latitude, longitude',
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.location_on)),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 300,
+                    child: TextFormField(
+                      controller: controllerlen,
+                      decoration: InputDecoration(
+                          labelText: 'Enter max height of the tree',
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.height)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  child: Text(
+                    "Generate QR code",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                      primary: Colors.green,
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Colors.green, width: 3.0),
+                          borderRadius: BorderRadius.circular(5.0))),
+                  onPressed: () async {
+                    if (controllerloc.text.isNotEmpty &&
+                        controllerlen.text.isNotEmpty &&
+                        controlleryear.text.isNotEmpty &&
+                        controllername.text.isNotEmpty) {
+                      String treeId = Uuid().v4();
+                      String treeName = controllername.text;
+                      String treeYear = controlleryear.text;
+                      String loc = controllerloc.text;
+                      String treeMaxLen = controllerlen.text;
 
-                FirebaseFirestore firestore = FirebaseFirestore.instance;
-                firestore.collection("trees").doc(treeId).set(
-                  {
-                    "treeId": treeId,
-                    "treeName": treeName,
-                    "treeYear": treeYear,
-                    "loc": loc,
-                    "treeMaxLen": treeMaxLen
-                  }
-                );
+                      FirebaseFirestore firestore = FirebaseFirestore.instance;
+                      firestore.collection("trees").doc(treeId).set({
+                        "treeId": treeId,
+                        "treeName": treeName,
+                        "treeYear": treeYear,
+                        "loc": loc,
+                        "treeMaxLen": treeMaxLen
+                      });
 
-                String data = "";
-                data+=treeId+", ";
-                data+=controllerloc.text+", ";
-                data+=controllerlen.text;
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> QRShower(data)));
-              }
-            },
-            shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.green, width: 3.0),
-                borderRadius: BorderRadius.circular(20.0)
+                      String data = "";
+                      data += treeId + ", ";
+                      data += controllerloc.text + ", ";
+                      data += controllerlen.text;
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => QRShower(data)));
+                    } else {
+                      return showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              content: Container(
+                                height: 85,
+                                child: Padding(
+                                  padding: EdgeInsets.only(),
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        "Please enter data",
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text('Ok'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          });
+                    }
+                  },
+                )
+              ],
             ),
-          )
-
+          ),
         ],
       ),
     );
-
   }
 }
--- lib/components/myflatButton.dart
+++ lib/components/myflatButton.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-class MyFlatButton extends StatelessWidget{
-
+class MyFlatButton extends StatelessWidget {
   String text;
   Widget widget;
 
-  MyFlatButton(String text, Widget widget){
-    this.text=text;
+  MyFlatButton(String text, Widget widget) {
+    this.text = text;
     this.widget = widget;
   }
 
@@ -16,19 +15,19 @@ class MyFlatButton extends StatelessWidget{
       padding: const EdgeInsets.all(8.0),
       child: FlatButton(
         padding: EdgeInsets.all(15.0),
-        onPressed: () async{
-          Navigator.of(context).push(MaterialPageRoute(builder: (context)=> widget));
+        onPressed: () async {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => widget));
         },
         child: Text(
           text,
-          style: TextStyle(color: Colors.lightGreen, fontWeight: FontWeight.bold),
+          style:
+              TextStyle(color: Colors.lightGreen, fontWeight: FontWeight.bold),
         ),
         shape: RoundedRectangleBorder(
             side: BorderSide(color: Colors.green, width: 3.0),
-            borderRadius: BorderRadius.circular(20.0)
-        ),
+            borderRadius: BorderRadius.circular(10.0)),
       ),
     );
   }
-
 }
