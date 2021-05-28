0b212d99e7869534ea4ab89c253d5c02d658de9e
sec19it089 <sec19it089@sairamtap.edu.in>
2021-05-28 23:45:34 +0530

    Added onboarding page

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,183 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'dart:io';
+import 'package:forest_tagger/components/WelComePage.dart';
+import 'package:forest_tagger/components/signUp.dart';
+import 'package:forest_tagger/data/data.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-import 'package:forest_tagger/components/AuthHandler.dart';
-
-
-
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Home(),
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: loggedInChecking(),
     );
   }
 }
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List<SliderModel> mySLides = new List<SliderModel>();
+  int slideIndex = 0;
+  PageController controller;
+
+  Widget _buildPageIndicator(bool isCurrentPage){
+    return Container(
+     margin: EdgeInsets.symmetric(horizontal: 2.0),
+      height: isCurrentPage ? 10.0 : 6.0,
+      width: isCurrentPage ? 10.0 : 6.0,
+      decoration: BoxDecoration(
+        color: isCurrentPage ? Colors.grey : Colors.grey[300],
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    mySLides = getSlides();
+    controller = new PageController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [const Color(0xff3C8CE7), const Color(0xff00EAFF)])),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          height: MediaQuery.of(context).size.height - 100,
+          child: PageView(
+            controller: controller,
+              onPageChanged: (index) {
+                setState(() {
+                  slideIndex = index;
+                });
+              },
+          children: <Widget>[
+            SlideTile(
+              imagePath: mySLides[0].getImageAssetPath(),
+              title: mySLides[0].getTitle(),
+              desc: mySLides[0].getDesc(),
+            ),
+            SlideTile(
+              imagePath: mySLides[1].getImageAssetPath(),
+              title: mySLides[1].getTitle(),
+              desc: mySLides[1].getDesc(),
+            ),
+            SlideTile(
+              imagePath: mySLides[2].getImageAssetPath(),
+              title: mySLides[2].getTitle(),
+              desc: mySLides[2].getDesc(),
+            )
+          ],
+          ),
+        ),
+        bottomSheet: slideIndex != 2 ? Container(
+          margin: EdgeInsets.symmetric(vertical: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              // ignore: deprecated_member_use
+              FlatButton(
+                onPressed: (){
+                  controller.animateToPage(2, duration: Duration(milliseconds: 400), curve: Curves.linear);
+                },
+                splashColor: Colors.blue[50],
+                child: Text(
+                  "SKIP",
+                  style: TextStyle(color: Color(0xFF3E2723), fontWeight: FontWeight.w600),
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    for (int i = 0; i < 3 ; i++) i == slideIndex ? _buildPageIndicator(true): _buildPageIndicator(false),
+                  ],),
+              ),
+              // ignore: deprecated_member_use
+              FlatButton(
+                onPressed: (){
+                  print("this is slideIndex: $slideIndex");
+                  controller.animateToPage(slideIndex + 1, duration: Duration(milliseconds: 500), curve: Curves.linear);
+                },
+                splashColor: Colors.blue[50],
+                child: Text(
+                  "NEXT",
+                  style: TextStyle(color: Color(0xFF3E2723), fontWeight: FontWeight.w600),
+                ),
+              ),
+            ],
+          ),
+        ): InkWell(
+          onTap: (){
+                Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Welcome()),
+            );            
+          },
+          child: Container(
+            
+            height: Platform.isIOS ? 70 : 60,
+            color: Colors.lightGreen,
+            alignment: Alignment.center,
+            child: Text(
+              "LETS GO!!",
+              style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600),),
+              
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class SlideTile extends StatelessWidget {
+  String imagePath, title, desc;
+
+  SlideTile({this.imagePath, this.title, this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(imagePath),
+          SizedBox(
+            height: 40,
+          ),
+          Text(title, textAlign: TextAlign.center,style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 20
+          ),),
+          SizedBox(
+            height: 20,
+          ),
+          Text(desc, textAlign: TextAlign.center,style: TextStyle(
+          fontWeight: FontWeight.w500,
+              fontSize: 14))
+        ],
+      ),
+    );
+  }
+}
+
